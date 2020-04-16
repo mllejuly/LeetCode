fe06005b34459303079b8df7de678d26fe0fcbb0
Erin Fang <qifangerin@gmail.com>
2020-04-16 18:34:44 -0400

    Update 001. Two Sum.java

--- LeetCode_Solution/001. Two Sum.java
+++ LeetCode_Solution/001. Two Sum.java
@@ -1,20 +1,37 @@
 //////////////////////////////////////////////////////////////////////////////////////
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        // 1. Initialize HashMap
+        //    key: nums[i]
+        //    value: i
         Map<Integer, Integer> map = new HashMap<>();
+        
+        // 2. Iterate the nums array
         for (int i = 0; i < nums.length; i++) {
-            int complement = target - nums[i];
-            if (map.containsKey(complement)) {
-                return new int[] {map.get(complement), i};
+            // 2-1. Record curNum and curComplemrnt
+            int curNum = nums[i];
+            int curComplement = target - curNum;
+            // 2-2. Check the existence of curComplement
+            if (map.containsKey(curComplement)) {
+                // 2-2-1. curComplement find: return index
+                return new int[] {map.get(curComplement), i};
+            } else {
+                // 2-2-2. curComplement not find: add to map and continue
+                map.put(curNum, i);
             }
-            map.put(nums[i], i);
         }
+        
+        // 3. No complement find for entire array: throw Exception info
         throw new IllegalArgumentException("No two sum solution");
+        
     }
 }
+
+// time:  O(n)
+// space: O(n)
+
 //////////////////////////////////////////////////////////////////////////////////////
-//  Time : O(n)
-// Space : O(n)
 
     
 // BruteForce
