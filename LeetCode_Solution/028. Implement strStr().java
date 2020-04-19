b19c9e01028f7683576fa19b3bb3a97a7cb1e0b0
Erin Fang <qifangerin@gmail.com>
2020-04-19 00:25:43 -0400

    Update 028. Implement strStr().java

--- LeetCode_Solution/028. Implement strStr().java
+++ LeetCode_Solution/028. Implement strStr().java
@@ -1,40 +1,19 @@
-// 脸书,字符串处理,苹果,微软,基本实现,Pocket Gems
-
-
 class Solution {
     public int strStr(String haystack, String needle) {
+        int n = haystack.length();
+        int m = needle.length();
         
-        // 1. boundary case
-        if (haystack.length() == 0 && needle.length() == 0) {
-            return 0;
-        }
-        
-        if (haystack.length() < needle.length()) {
-            return -1;
-        }
-        
-        
-        // 2. 双指针比较
-        // 注意 i 的范围
-        for (int i = 0; i < haystack.length() - needle.length() + 1; i++) {
-            int j = 0;
-            for (j = 0; j < needle.length(); j++) {
-                // 比较 haystack 和 needle 当前字符串是否相等
-                // 如果不相等则移动 haystack 指针 i, 找到相同的之后再持续比较后续字符
-                if (haystack.charAt(i + j) != needle.charAt(j)) {
-                    break;
-                }
-                
-            }
-            
-            // 如果符合的长度等于needle的长度，则说明 haystack包含完整的 needle
-            // 说明找到，返回当前 haystack 的指针位置 i
-            if (j == needle.length()) {
+        for (int i = 0; i < n - m + 1; i++) {
+            if (haystack.substring(i, i + m).equals(needle)) {
                 return i;
             }
         }
         
-        // 3. 没有找到子字符串返回 -1
         return -1;
+        
     }
 }
+
+
+// time : O(m * n)
+// space: O(1)
