b9bef4637db5776b230a6af9b8e7dfa39d5d9c07
Erin Fang <qifangerin@gmail.com>
2020-05-08 23:01:32 -0400

    Update 065. Valid Number.java

--- LeetCode_Solution/065. Valid Number.java
+++ LeetCode_Solution/065. Valid Number.java
@@ -1,16 +1,19 @@
 class Solution {
+    
     public boolean isNumber(String s) {
         
+        // skip heading/trailing whitespace
         s = s.trim();
-
-        boolean dec = false;
-        boolean exp = false;
+        
         boolean num = false;
+        boolean exp = false;
         boolean numAfterExp = false;
+        // boolean sign = false;
+        boolean dec = false;
         
-        for(int i = 0; i < s.length(); i++) {
-        
-            if('0' <= s.charAt(i) && s.charAt(i) <= '9') {
+        for (int i = 0; i < s.length(); i++) {
+            
+            if ('0' <= s.charAt(i) && s.charAt(i) <= '9') {
                 // valid num
                 if (exp) {
                     numAfterExp = true;
@@ -23,27 +26,28 @@ class Solution {
                 }
                 dec = true;
             } else if (s.charAt(i) == 'e') {
-                // valid exp: no exp before cur exp; cannot appear before num
+                // valid exp: no exp before cur exp; must be followed after a number
                 if (exp || !num) {
                     return false;
                 }
                 numAfterExp = false;
                 exp = true;
-            } else if (s.charAt(i) == '-' || s.charAt(i) == '+') {
-                // valid sign: can only appear in the beginning; cannot follow directly after exp
+            } else if (s.charAt(i) == '+' || s.charAt(i) == '-') {
+                // valid sign: can only appear at 0; cannot follow directly after exp
                 if (i != 0 && s.charAt(i - 1) != 'e') {
                     return false;
                 }
             } else {
                 return false;
             }
+            
         }
-
+        
         // valid num with valid exp or no exp
         return num && ( (exp && numAfterExp) || (!exp && !numAfterExp) );
+        
     }
 }
-
 
 // time : O(n)
 // space: O(1)
