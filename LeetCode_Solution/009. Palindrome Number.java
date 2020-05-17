c286351fbbf832c14e4be419fda80d0805f689c0
Erin Fang <qifangerin@gmail.com>
2020-05-16 23:50:56 -0400

    Update 009. Palindrome Number.java

--- LeetCode_Solution/009. Palindrome Number.java
+++ LeetCode_Solution/009. Palindrome Number.java
@@ -1,24 +1,16 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        
-        // 取x的绝对值
-        int a = 0;
-        a = Math.abs(x);
-
-        // 算出x的reverse number
-        int rev = 0;
-        while (a != 0) {
-            rev = rev * 10 + a % 10;
-            a /= 10;
-        }
-        
-        // 在x > 0 且 x == rev才返回T
-        if (x >= 0 && x == rev) {
-            return true;
-        }
-        // x < o 返回F
-        else {
+        if (x < 0) {
             return false;
         }
+        
+        int num = x;
+        int rev = 0;
+        while (num != 0) {
+            rev = rev * 10 + num % 10;
+            num /= 10;
+        }
+        return rev == x;
+        
     }
 }
