d6f573fdff14ffa1d91a5bb912f3737ed4a9885b
Erin Fang <qifangerin@gmail.com>
2020-05-18 00:40:12 -0400

    Update 024. Swap Nodes in Pairs.java

--- LeetCode_Solution/024. Swap Nodes in Pairs.java
+++ LeetCode_Solution/024. Swap Nodes in Pairs.java
@@ -27,3 +27,46 @@ class Solution {
 
 // time : O(n)
 // space: O(n)
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+// Iteration
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        ListNode dummyHead = new ListNode();
+        dummyHead.next = head;
+        ListNode cur = dummyHead;
+        
+        while (head != null && head.next != null) {
+            ListNode oddNode = head;
+            ListNode evenNode = head.next;
+            
+            // swap
+            cur.next = evenNode;
+            oddNode.next = evenNode.next;
+            evenNode.next = oddNode;
+            
+            cur = oddNode;
+            head = oddNode.next;
+        }
+        
+        return dummyHead.next;
+    }
+}
+
+// time : O(n)
+// space: O(1)
+
+
