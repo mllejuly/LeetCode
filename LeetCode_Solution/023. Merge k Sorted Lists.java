b19c459a19162b3a51da4f9b9c7406da35144af9
Erin Fang <qifangerin@gmail.com>
2020-05-18 01:05:33 -0400

    Create 023. Merge k Sorted Lists.java

---
+++ LeetCode_Solution/023. Merge k Sorted Lists.java
@@ -0,0 +1,64 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+// MergeSort | Divide and Conquer
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0) {
+            return null;
+        }
+        
+        return mergeKLists(lists, 0, lists.length - 1);
+    }
+    
+    
+    private ListNode mergeKLists(ListNode[] lists, int l, int r) {
+        if (l == r) {
+            return lists[l];
+        } else if (l < r) {
+            int mid = l + (r - l) / 2;
+            ListNode leftList = mergeKLists(lists, l, mid);
+            ListNode rightList = mergeKLists(lists, mid + 1, r);
+            return mergeTwoLists(leftList, rightList);
+        }    
+        return null;
+    }
+    
+    private ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode();
+        ListNode cur = dummyHead;
+        
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                cur.next = l1;
+                l1 = l1.next;
+            } else {
+                cur.next = l2;
+                l2 = l2.next;
+            }
+            cur = cur.next;
+        }
+        
+        if (l1 != null) {
+            cur.next = l1;
+        }
+        if (l2 != null) {
+            cur.next = l2;
+        }
+        
+        return dummyHead.next;
+    }
+    
+}
+
+
+// time : O(n log k)
+// space: O(1)
