a6a17760d5e90e14c6027ca448125eede73b3f8e
Erin Fang <qifangerin@gmail.com>
2020-05-19 01:34:16 -0400

    Update 098. Validate Binary Search Tree.java

--- LeetCode_Solution/098. Validate Binary Search Tree.java
+++ LeetCode_Solution/098. Validate Binary Search Tree.java
@@ -36,3 +36,52 @@ class Solution {
 
 // time : O(n)
 // space: O(n)
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+
+// iteration
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        Stack<TreeNode> stack = new Stack();
+        
+        double preRight = -Double.MAX_VALUE;
+        while (!stack.isEmpty() || root != null) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            
+            root = stack.pop();
+            
+            if (root.val <= preRight) {
+                return false;
+            }
+            preRight = root.val;
+            root = root.right;
+        }
+        
+        return true;
+    }
+    
+}
+
+// time : O(n)
+// space: O(n)
