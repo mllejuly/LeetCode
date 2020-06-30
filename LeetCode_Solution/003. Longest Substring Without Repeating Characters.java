b96b6ab0ad2c949d9164439b3cbe586ec408eefa
Erin Fang <qifangerin@gmail.com>
2020-06-30 12:06:21 -0700

    Update 003. Longest Substring Without Repeating Characters.java

--- LeetCode_Solution/003. Longest Substring Without Repeating Characters.java
+++ LeetCode_Solution/003. Longest Substring Without Repeating Characters.java
@@ -1,30 +1,25 @@
-//////////////////////////////////////////////////////////////////////////////////////
 class Solution {
     public int lengthOfLongestSubstring(String s) {
         
-        Set<Character> set = new HashSet<>();
-        int result = 0;
-        int leftIdx = 0;
-        int rightIdx = 0;
+        Set<Character> charSet = new HashSet<>();
+        int windowLength = 0;
+        int l = 0;
+        int r = 0;
         
-        int n = s.length();
-        while (rightIdx < n) {
-            
-            if (!set.contains(s.charAt(rightIdx))) {
-                set.add(s.charAt(rightIdx));
-                rightIdx++;
-                result = Math.max(result, rightIdx - leftIdx);
+        while (r < s.length()) {
+            if (!charSet.contains(s.charAt(r))) {
+                charSet.add(s.charAt(r));
+                r++;
+                windowLength = Math.max(windowLength, r - l);
             } else {
-                set.remove(s.charAt(leftIdx));
-                leftIdx++;
+                charSet.remove(s.charAt(l));
+                l++;
             }
-            
         }
         
-        return result;
+        return windowLength;
     }
 }
-//////////////////////////////////////////////////////////////////////////////////////
 
 // Sliding Window
 // time : O(n)
