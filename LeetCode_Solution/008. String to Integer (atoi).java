f8c68b7df87203d73ed90f7b6ae8fc5d98fd0cc6
Erin Fang <qifangerin@gmail.com>
2020-06-30 19:12:27 -0700

    Update 008. String to Integer (atoi).java

--- LeetCode_Solution/008. String to Integer (atoi).java
+++ LeetCode_Solution/008. String to Integer (atoi).java
@@ -1,44 +1,39 @@
 class Solution {
-    public int myAtoi(String str) {
-        
-        // 1. skip heading whitespaces
-        str = str.trim();
 
-        int i = 0;
-        int n = str.length();
+    public int myAtoi(String str) {
+
+        str = str.trim();
         
-        // 2. handle sign
+        // handle sign
         int sign = 1;
-        if (i < n && str.charAt(i) == '+') {
+        int i = 0;
+        if (i < str.length() && str.charAt(i) == '+') {
             i++;
-            if (str.length() - 1 < i || !Character.isDigit(str.charAt(i))) {        
+            if (i > str.length() - 1 || !Character.isDigit(str.charAt(i))) {        
                 return 0;
             }
         }
-        if (i < n && str.charAt(i) == '-') {
+        if (i < str.length() && str.charAt(i) == '-') {
             sign = -1;
             i++;
-            if (str.length() - 1 < i || !Character.isDigit(str.charAt(i))) {        
+            if (i > str.length() - 1 || !Character.isDigit(str.charAt(i))) {        
                 return 0;
             }
         }
         
-        // 3. get number by digit
+        // get number
         long num = 0;
-        while (i < n && Character.isDigit(str.charAt(i))) {
+        while (i < str.length() && Character.isDigit(str.charAt(i))) {
             num = num * 10 + Character.getNumericValue(str.charAt(i));
             i++;
-            
-            // handle overflow [-2147483648, 2147483647]
+            // handle overflow
             if (num > Integer.MAX_VALUE) {
                 return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
             }
-            
         }
         
         int result = (int)num;
         return sign * result;
-        
     }
 }
 
